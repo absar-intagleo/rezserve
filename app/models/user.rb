42c3d35bd9eb991c42b2861253150d53359291ed
absar-mushtaq <absar.mushtaq@ispokepakistan.com>
2018-07-27 12:40:07 +0500

    Presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   belongs_to :user_type
   has_many :properties, dependent: :destroy
 
+  validates_presence_of :email, :dt_uuid, :first_name, :last_name
   validates_uniqueness_of :email, :scope => :user_type_id
 end
