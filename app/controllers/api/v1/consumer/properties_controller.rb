79c6e4a7d21d773c44fca73db33339314201ab10
absar-mushtaq <absar.mushtaq@ispokepakistan.com>
2018-07-31 17:22:25 +0500

    Property namespace error

--- app/controllers/api/v1/consumer/properties_controller.rb
+++ app/controllers/api/v1/consumer/properties_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::Consumer::PropertiesController < Api::V1::Consumer::BaseControlle
 	end
 
 	def show
-		@property = Property.find(params[:property_id]) rescue nil
+		@property = Property.find(params[:property_id])
 		if @property.present?			
 			render 'save_property_information'
 		else
